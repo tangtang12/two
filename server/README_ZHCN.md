22572ee645673ccd06e7056071f5d15aaa5e6647
tangtang12 <986511087@qq.com>
2018-07-03 13:00:50 +0800

    'HOME的数据'

---
+++ server/README_ZHCN.md
@@ -0,0 +1,25 @@
+## HOME
+> 一个接口获取不同的数据
+- /home/query
+- 问号传参 lx=xxx 根据lx的值传递不同类型的数据
+> lx可以参考的值(页面从上往下)
+- SLIDE 主要轮播图
+- ICONS_LIST 4个主要的icon图片
+- BANNER_LIST 需要展示的图片
+- HOT_CATEGORY 热门品类
+- BANNER_SLIDE 小轮播图
+- HOT_BRANDS 热门品牌
+- HOT_SINGLE_GOODS 人气推荐
+- MAYBE_LIKE 可能喜欢
+> 得到数据({code: 0, msg: "OK!", data: []代表成功)
+- data 的具体说明(需要就获取)
+```
+"id": x,
+"pic": 图片地址,
+"desc": 图片描述
+"price":价格,
+"oldPrice":原先的价格
+"view":多少人在浏览,
+"state":"正在浏览",
+
+```
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import axios from 'axios';
+import Qs from 'qs';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+
+    axios.defaults.baseURL = 'http://localhost:8000';
+    axios.defaults.withCredentials = true;//允许跨域(并且携带cookie)
+    axios.defaults.transformRequest = (data = {}) => Qs.stringify(data);//把post/put请求主体传递给服务器的内容统一处理为x-www-url-encoded格式
+    axios.interceptors.response.use(result => result.data);//响应拦截器：把服务器返回的信息中响应主体内容拦截返回，以后在then中获取的结果就是主体内容
+
+   axios.get('/home/query?lx=BANNER_LIST').then(result=>{
+        console.log(result);
+    });
+
+axios.get('/home/query?lx=SLIDE').then(result=>{
+    console.log(result);
+});
