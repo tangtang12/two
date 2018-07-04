08a4e8a404bf1348697b7aa18f3555f146030766
tangtang12 <986511087@qq.com>
2018-07-04 12:24:44 +0800

    'wonder页面'

--- src/routes/wander/SLIDE.js
+++ src/routes/wander/SLIDE.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Carousel} from 'antd';
+import queryData from "../../api/wander";
+
 
 class Slide extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {
+            slideData: []
+        }
     }
 
-    async componentDidMount(){
-
+    async componentDidMount() {
+        let res = await  queryData('SLIDE');
+        if (res.code===0){
+            let {data}=res;
+            this.setState({
+                slideData:data
+            })
+        }
     }
 
     render() {
-        return <div>
-
+        let {slideData} =this.state;
+        if (slideData.length===0)return'';
+        return <div className='wanderSlide'>
+            <Carousel>
+                {slideData.map((item,index)=>{
+                    let {pic,desc}=item;
+                    return <div key={index}><img src={pic} alt={desc}/></div>
+                })}
+            </Carousel>
         </div>
     }
 }
