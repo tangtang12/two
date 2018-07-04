9d2ae09ce8a6ed3701fdec0ce9f0042cec60da23
tangtang12 <986511087@qq.com>
2018-07-04 16:10:07 +0800

    'classify样式'

--- src/routes/classify/List.js
+++ src/routes/classify/List.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import {connect} from 'react-redux'
-import {queryMenList} from '../../../api/classify'
-import {Link} from 'react-router-dom'
-
+import {queryMenList} from '../../api/classify'
+import Right from './Right';
 class List extends React.Component {
+
     constructor(props, context) {
         super(props, context);
         this.state = {
-            keys: [], index: 0
+            keys: [], index: 0,data:[]
         }
     }
 
     async componentWillMount() {
-        let result = await queryMenList('kidsList');
+        let result = await queryMenList(this.props.type);
         this.data = result.data;
         let keys = [];
         for (let key in result.data) {
             keys.push(key);
-        };
+        }
+
         this.keys = keys;
         this.setState({
             keys
         });
-        console.log(result);
     }
 
     render() {
         let keys = this.state.keys;
         if (keys.length === 0) return "";
-        return <div>
-            <div className='menListBox'>
+        return <div className='menListBox'>
                 <ul>
                     {keys.map((item, index) => {
                         return <li key={index}><a onClick={() => {
@@ -40,27 +39,17 @@ class List extends React.Component {
                         }>{item}</a></li>
                     })}
                 </ul>
+
+            <div className='right'>
+                <Right data={this.fn(this.state.index)}/>
             </div>
-
-            <span>—热门推荐—</span>
-            <div className='menImgBox'>
-
-                <div><Link to='/classify'><img src="//gw.alicdn.com/bao/uploaded/TB15UXtXhZIL1JjSZFNwu0upXXa.png_q75.jpg"
-                                               alt=""/></Link></div>
-            </div>
-
-
         </div>
-
     }
 
-    fn = () => {
-        let data = this.data[this.state.keys[this.state.index]];
-        for (let attr in data) {
-            console.log(data[attr]);
-            return attr;
-        }
+    fn = (index) => {
+        return this.data[this.state.keys[index]];
     }
+
 
 }
 
---
+++ src/routes/classify/Right.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+class Right extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let data=this.props.data;
+        if (!data)return '';
+        let attrAry=[];
+        for (let attr in data) {
+            attrAry.push(attr);
+        }
+        return <div className='imgBox'>
+            {attrAry.map((item,index)=>{
+             return   <div className="step clearfix" key={index}>
+                    <h3>--{item}--</h3>
+                    <div className="imgBox">
+                        {data[item].map((item, index) => {
+                        let {pic, desc} = item;
+                        return <div className="smallBox" key={index}>
+                                <img src={pic} alt=""/>
+                                <p>{desc}</p>
+                               </div>
+                    })}
+                    </div>
+                </div>
+            })}
+        </div>
+    }
+}
+
+export default connect()(Right);
