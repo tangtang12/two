83af6dbe15d3ce1c0743a8a25a5544bbd3390b60
tangtang12 <986511087@qq.com>
2018-07-05 12:17:12 +0800

    '商品详情路由'

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -9,36 +9,48 @@ import queryCommodity from '../api/commodity';
 class Details extends React.Component {
     constructor(props,context) {
         super(props,context);
+        this.state={
+            data:[],
+            id:''
+        }
     }
 
-    async  componentWillMount(){
+    async componentDidMount(){
         let str =this.props.location.search.slice(1),
         obj=Qs.parse(str);
-        this.data=await queryCommodity(obj.id);
-        console.log(this.data);
+        let res=await queryCommodity(obj.id);
+        if (res.code===0){
+            this.setState({
+                id:obj.id,
+                data:res.data
+            })
+        }
+
     }
     render() {
-        console.log(this.data);
+        console.log(this.state.data);
+        let data=this.state.data;
+       if (data.length===0)return "";
+       let {pic,name,price,hot,shop,shopDesc} =data;
         return <section className="detailsBox">
             <div className="priceSwipe">
                 <Carousel autoplay>
-                    <div className={"imgBox"}><img src="//img11.static.yhbimg.com/goodsimg/2017/10/16/13/019095f124f0be54825f5edf15b7d02e98.jpg?imageMogr2/thumbnail/450x600/background/d2hpdGU=/position/center/quality/60" alt=""/></div>
-                    <div className={"imgBox"}><img src="//img11.static.yhbimg.com/goodsimg/2017/10/16/13/019095f124f0be54825f5edf15b7d02e98.jpg?imageMogr2/thumbnail/450x600/background/d2hpdGU=/position/center/quality/60" alt=""/></div>
-                    <div className={"imgBox"}><img src="//img11.static.yhbimg.com/goodsimg/2017/10/16/13/019095f124f0be54825f5edf15b7d02e98.jpg?imageMogr2/thumbnail/450x600/background/d2hpdGU=/position/center/quality/60" alt=""/></div>
-                    <div className={"imgBox"}><img src="//img11.static.yhbimg.com/goodsimg/2017/10/16/13/019095f124f0be54825f5edf15b7d02e98.jpg?imageMogr2/thumbnail/450x600/background/d2hpdGU=/position/center/quality/60" alt=""/></div>
+                    {pic.map((item,index)=>{
+                       return <div className={"imgBox"} key={index}><img src={item} alt=""/></div>
+                    })}
                 </Carousel>
             </div>
             <div className="goods-name">
-                <h1 className="name">I LOVE CHOC YOHO！GIRL联名款  织带拼接短款卫衣   粉色</h1>
+                <h1 className="name">{name}</h1>
             </div>
             <div className="goods-price">
-                <h2 className="price">¥999.00</h2>
+                <h2 className="price">¥{price}</h2>
             </div>
 
             <div className="goods-discount">
                 <h2 className="first-item">
                     <span>促</span>
-                    【嗨购一夏】满￥599享7.5折
+                    {hot}
                 </h2>
                 <Icon type="down" />
             </div>
@@ -71,8 +83,8 @@ class Details extends React.Component {
 
             <div className="enter-store">
                 <a href="#" className="store-logo"><img
-                    src="//img11.static.yhbimg.com/yhb-img01/2017/11/07/16/01328c3cc9ae18b7c5454144a378692e77.jpg?imageMogr2/thumbnail/47x47/extent/47x47/background/d2hpdGU=/position/center/quality/80" alt=""/></a>
-                <a href="#" className="store-name">I LOVE CHOC</a>
+                    src={shop} alt=""/></a>
+                <a href="#" className="store-name">{shopDesc}</a>
                 <a href="#" className="store-link">进入店铺<Icon type="right"/></a>
             </div>
 
