9bc33cf5d08023255376691b56084f96361c8de6
fenggenggeng <2498421176@qq.com>
2018-07-06 15:54:14 +0800

    坐沙发

--- src/routes/cartLogin/CartLogin.js
+++ src/routes/cartLogin/CartLogin.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import {Link, withRouter} from "react-router-dom"
-import {connect} from "react-redux";
+import React, { Component } from 'react';
+import { Link, withRouter } from "react-router-dom"
+import { connect } from "react-redux";
 import action from "../../store/action";
 import "./CartLogin.less"
 import {
     Icon,
     Button,
-    
+
 } from 'antd'
 import Box from "../../component/Box";
 import {
-    getCart, modify
+    getCart, modify, selected
 } from "../../api/car";
 
 class Collapsed extends React.Component {
@@ -33,17 +33,17 @@ class Collapsed extends React.Component {
     };
 
     render() {
-        let {num, desc, name, price, id} = this.props;
+        let { num, desc, name, price, id } = this.props;
         return <a href="javascript:;">
             <div className='tip-shop-2'>x{
                 this.state.num
             }</div>
             {this.props.collapsed ? <ul className="desc-shop-f">
-                    <li className='tip-shop-1'>{name}</li>
-                    <li className='tip-shop-3'>{desc}</li>
-                    <li className='tip-shop-4'>￥{price}</li>
-                </ul> :
-                <div><Box id={id} modify={this.modify} num={num} max={10} min={0} getNum={this.getNum}/>
+                <li className='tip-shop-1'>{name}</li>
+                <li className='tip-shop-3'>{desc}</li>
+                <li className='tip-shop-4'>￥{price}</li>
+            </ul> :
+                <div><Box id={id} modify={this.modify} num={num} max={10} min={0} getNum={this.getNum} />
                 </div>}
         </a>
     }
@@ -81,21 +81,21 @@ class CartLogin extends React.Component {
             {/*商品信息和价格*/}
 
             {this.state.getInfo.length === 0 ? "" : this.state.getInfo.map((item, index) => {
-                let {hot, pic = [], name, num, desc, price, id} = item;
+                let { hot, pic = [], name, num, desc, price, id,size, color} = item;
 
                 return (<div key={index}>
                     <div className='tip-plus tip2'>
 
-                        <Icon type="tag-o"/>
+                        <Icon type="tag-o" />
                         <span>{hot}</span>
                         <Link to='/classify'>去凑单 &gt; </Link>
                     </div>
                     <div className='shop_f'>
-                        <div className="select-f" onClick={this.selectedOne}>
-                            {this.state.selected || this.state.selectedTwo ? <Icon type='check'/> : ''}
+                        <div className="select-f" onClick={this.selectedOne.bind(this,{id,num,size,color})}>
+                            {this.state.selected || this.state.selectedTwo ? <Icon type='check' /> : ''}
                         </div>
 
-                        <img src={pic[0]}/>
+                        <img src={pic[0]} />
 
                         <a href="javascript:;">
 
@@ -109,22 +109,22 @@ class CartLogin extends React.Component {
                                 <li className='tip-shop-1'>{name}</li>
                                 <li className='tip-shop-3'>{desc}</li>
                                 <li className='tip-shop-4'>￥{price}</li>
-                            </ul> : <div><Box id={id} modify={this.modify} num={num}  max={10} min={0} getNum={this.getNum}/>
-                            <input type='text' />  
+                            </ul> : <div><Box id={id} modify={this.modify} num={num} max={10} min={0} getNum={this.getNum} />
 
-                            </div>}
-                        </a>   
-                         <Collapsed num={num} name={name} desc={desc} price={price} id={id}
-                                   collapsed={this.props.collapsed}/>                    </div>
+
+                                </div>}
+                        </a>
+                        <Collapsed num={num} name={name} desc={desc} price={price} id={id}
+                            collapsed={this.props.collapsed} />                    </div>
                 </div>)
             })}
 
             {this.props.collapsed ?
                 <div>
                     <div className='message-f'>
-                        <Icon type="pay-circle-o"/>
+                        <Icon type="pay-circle-o" />
                         <span>全场加价购</span>
-                        <a href="#">去换购 <p style={{color: "#bfbfbf"}}>&gt;</p></a>
+                        <a href="#">去换购 <p style={{ color: "#bfbfbf" }}>&gt;</p></a>
 
                     </div>
                     <div className='price-all' style={{
@@ -133,8 +133,8 @@ class CartLogin extends React.Component {
                         lineHeight: ".9rem",
                         background: "#fff"
                     }}>
-                <span style={{margin: ".2rem", fontSize: ".3rem"}}>
-                总计
+                        <span style={{ margin: ".2rem", fontSize: ".3rem" }}>
+                            总计
                 </span>
                     </div>
                 </div> : ""}
@@ -142,7 +142,7 @@ class CartLogin extends React.Component {
 
             {/*底部*/}
 
-            {this.props.collapsed ? <div className="login-all" style={{zIndex: "99999"}}>
+            {this.props.collapsed ? <div className="login-all" style={{ zIndex: "99999" }}>
                 <div>
                     <a href="javascript:;" style={{
                         color: '#000',
@@ -156,7 +156,7 @@ class CartLogin extends React.Component {
 
                     }} onClick={this.selectedAll}>
 
-                        {this.state.selected ? <Icon type='check'/> : ''}
+                        {this.state.selected ? <Icon type='check' /> : ''}
 
                     </a>
                     <p>全选</p>
@@ -169,31 +169,31 @@ class CartLogin extends React.Component {
                     }}>不含运费</p>
                     <Button>结算</Button>
                 </div>
-            </div> : <div className="login-all" style={{zIndex: "99999"}}>
-                <div>
-                    <a href="javascript:;" style={{
-                        color: '#000',
-                        border: '.02rem solid',
-                        width: '.4rem',
-                        height: '.4rem',
-                        position: 'absolute',
-                        top: '.2rem',
-                        left: '.2rem',
-                        borderRadius: '50%'
-                    }} onClick={this.selectedAll}>
-                        {this.state.selected ? <Icon type='check'/> : ''}
+            </div> : <div className="login-all" style={{ zIndex: "99999" }}>
+                    <div>
+                        <a href="javascript:;" style={{
+                            color: '#000',
+                            border: '.02rem solid',
+                            width: '.4rem',
+                            height: '.4rem',
+                            position: 'absolute',
+                            top: '.2rem',
+                            left: '.2rem',
+                            borderRadius: '50%'
+                        }} onClick={this.selectedAll}>
+                            {this.state.selected ? <Icon type='check' /> : ''}
 
-                    </a>
-                    <p>全选</p>
-                    <Button style={{
-                        background: "#555",
-                        right: "2.4rem"
-                    }}>移入
-                        <br/>
-                        收藏夹</Button>
-                    <Button>删除</Button>
+                        </a>
+                        <p>全选</p>
+                        <Button style={{
+                            background: "#555",
+                            right: "2.4rem"
+                        }}>移入
+                        <br />
+                            收藏夹</Button>
+                        <Button>删除</Button>
+                    </div>
                 </div>
-            </div>
 
 
             }
@@ -203,7 +203,7 @@ class CartLogin extends React.Component {
     }
 
     selectedAll = ev => {
-        console.log(ev.target.tagName);
+
 
         if (ev.target.tagName === 'A' || "I") {
             this.setState({
@@ -213,14 +213,17 @@ class CartLogin extends React.Component {
     };
 
 
-    selectedOne = ev => {
+    selectedOne = async (obj) => {
+        let res = await selected(obj)
+        console.log(res)
 
-        if (ev.target.tagName === "DIV" || "I") {
-            this.setState({
-                selectedTwo: !this.state.selectedTwo
-            });
 
-        }
+        // if (ev.target.tagName === "DIV" || "I") {
+        //     this.setState({
+        //         selectedTwo: !this.state.selectedTwo
+        //     });
+
+        // }
 
 
     };
